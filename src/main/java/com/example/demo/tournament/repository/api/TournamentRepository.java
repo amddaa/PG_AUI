3eb48e194f766c077f1c75732d79bfe8002c0d78
amddaa <slusarekadam1@gmail.com>
2023-10-16 23:14:52 +0200

    start microservices architecture

--- src/main/java/com/example/demo/tournament/repository/api/TournamentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.tournament.repository.api;
-import com.example.demo.tournament.entity.Tournament;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface TournamentRepository extends JpaRepository<Tournament, UUID> {
-    Optional<Tournament> findById(UUID id);
-}
