7d0c2e8e2223225e91609601108b75ad25adc03f
amddaa <slusarekadam1@gmail.com>
2023-11-22 09:25:48 +0100

    Create docker-compose and building all services script

---
+++ build.bat
@@ -0,0 +1,21 @@
+REM Builds all applications and all docker images using Dockerfile and tags it based on org.opencontainers.image.version
+REM label in Dockerfile.
+
+REM Script main function. Builds all applications and all docker images using Dockerfile and tags it based on
+REM org.opencontainers.image.version label in Dockerfile.
+
+cd .\tournament\
+call .\build.bat
+cd ..
+
+cd .\participant\
+call .\build.bat
+cd ..
+
+cd .\gateway\
+call .\build.bat
+cd ..
+
+cd .\js\
+call .\build.bat
+cd ..
