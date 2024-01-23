50aeb2831f0376a9a72cac14b160b1dad0e4cd4b
amddaa <slusarekadam1@gmail.com>
2024-01-23 10:08:19 +0100

    Add Eureka server and clients

--- gateway/src/main/java/pg/LabApplication.java
+++ gateway/src/main/java/pg/LabApplication.java
@@ -3,6 +3,7 @@ package pg;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import java.util.Collections;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LabApplication {
 
 	public static void main(String[] args) {
--- tournament/src/main/java/pg/LabApplication.java
+++ tournament/src/main/java/pg/LabApplication.java
@@ -3,12 +3,14 @@ package pg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LabApplication {
 
 	public static void main(String[] args) {
